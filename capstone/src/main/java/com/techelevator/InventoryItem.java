1927ca942da22c15868c8ac977021796ef3884b5
Al Nachman <a.nachman.42@gmail.com>
2018-10-11 11:56:58 -0400

    created new classes for vending machine project

---
+++ capstone/src/main/java/com/techelevator/InventoryItem.java
@@ -0,0 +1,22 @@
+package com.techelevator;
+
+import java.math.BigDecimal;
+
+public class InventoryItem {
+	//class properties
+	protected String productName;
+	protected String type;
+	protected BigDecimal price;
+	protected int inventoryRemaining;
+	
+	public InventoryItem () {
+		//set name, type, and price
+		//always set inventoryRemaining to 5
+	}
+	
+	//create Getters and Setters
+	//Setter for inventoryRemaining always -1 (only setting when buying an item)
+
+	//methods?
+	//make a toString method?
+}
