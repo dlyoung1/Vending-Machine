7ca4581a95f4be50f69ad197e34e78a59d58307f
Darren Young <dlyoung5@asu.edu>
2018-10-12 10:38:50 -0400

    unit tests Inventory moved and Purchase created

---
+++ capstone/src/test/java/com/techelevator/view/PurchaseMenuTest.java
@@ -0,0 +1,14 @@
+package com.techelevator.view;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PurchaseMenuTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
